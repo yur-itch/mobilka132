91535caab6980bc052ca69085763e72f5c0cb49d
bcgtrunner <210253731+bcgtrunner@users.noreply.github.com>
2026-03-31 18:18:16 +0700

    location update

--- app/src/main/java/com/example/mobilka132/data/location/LocationManager.kt
+++ app/src/main/java/com/example/mobilka132/data/location/LocationManager.kt
@@ -6,8 +6,12 @@ import androidx.compose.ui.geometry.Offset
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.location.Location
 import android.os.Looper
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
 import androidx.core.app.ActivityCompat
 import com.example.mobilka132.MainActivity
@@ -17,32 +21,46 @@ import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.Priority
 
 
-class LocationManager {
+class LocationManager(private val activity: Context, private val registry: ActivityResultRegistry) {
 
     val mapLocation : Offset?
         get() = worldLocation?.let { l -> Offset(((l.longitude - 84.932881) * 122800).toFloat(), (-(l.latitude - 56.4758) * 222000).toFloat()) ?: null }
     var worldLocation : Location? = null
-    var fusedLocationClient: FusedLocationProviderClient? = null
-    val activity : MainActivity
-
-    constructor(activity: MainActivity) {
-        this.activity = activity
+    private val fusedLocationClient by lazy {
+        LocationServices.getFusedLocationProviderClient(activity)
     }
 
-    fun init() {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
+    private val requestPermissionLauncher = registry.register(
+        "location_permission_key",
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            @SuppressLint("MissingPermission")
+            getLastLocation()
+        }
+    }
+
+    fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getLastLocation()
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
     fun getLastLocation() {
-        fusedLocationClient!!.lastLocation.addOnSuccessListener { location ->
-            requestNewLocationData()
-            if (location != null) {
-                val lat = location.latitude
-                val lon = location.longitude
-                Toast.makeText(activity, "Широта: $lat, Долгота: $lon", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(activity, "Не удалось получить локацию", Toast.LENGTH_SHORT).show()
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient?.lastLocation?.addOnSuccessListener { location ->
+                requestNewLocationData()
+                if (location != null) {
+                    val lat = location.latitude
+                    val lon = location.longitude
+                    Toast.makeText(activity, "Широта: $lat, Долгота: $lon", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(activity, "Не удалось получить локацию", Toast.LENGTH_SHORT).show()
+                    requestNewLocationData()
+                }
             }
         }
     }
@@ -66,5 +84,4 @@ class LocationManager {
             }, Looper.getMainLooper())
         }
     }
-
 }
