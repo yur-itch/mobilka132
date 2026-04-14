f534a4afc2886b8ac058a06e79ba0e5cb6ea1314
bambukkan <pokko10@mail.ru>
2026-04-14 19:15:10 +0700

    визуализация дерева

--- app/src/main/java/com/example/mobilka132/MainActivity.kt
+++ app/src/main/java/com/example/mobilka132/MainActivity.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
